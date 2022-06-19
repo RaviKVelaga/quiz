d94685fc64a8394a0a216d610b74dd542b932f7c
Ravi Velaga <rvelaga@opentable.com>
2022-06-20 06:42:05 +1000

    adding html,  css and script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var allQuestions = [{
+    question: "What keyword is used to check whether a given property is valid or not?",
+    options: ["1.in", "2.is in", "3.exists", "4.lies"],
+    answer: "1.in"
+},
+{
+    question: "What does the Javascript “debugger” statement do?",
+    options: ["1.It will debug all the errors in the program at runtime.",
+        "2.It acts as a breakpoint in a program.",
+        "3.NIt will debug error in the current statement if any.",
+        "4.All of the above"],
+    answer: "2.It acts as a breakpoint in a program.",
+},
+{
+    question: "Javascript is an __________language?",
+    options: ["1.Object-Oriented", "2.Object-Based", "3.Procedural", "4.None of the above"],
+    answer: "1.Object-Oriented"
+},
+{
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    options: ["1.var", "2.let", "3.Both A and B", "4.None of the above"],
+    answer: "3.Both A and B"
+}];
+
